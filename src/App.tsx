db6ca0a5768824b779b14bec34ede68e278af8bb
Kerim Kutuk <kutukmu@gmail.com>
2021-04-19 09:56:57 -0500

    sematic ui and cors policy set up, ready to go

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    const data = axios
+      .get("http://localhost:5000/api/Activities")
+      .then((res) => {
+        console.log(res);
+        setActivities(res.data);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header icon="users" content="Welcome to the My new Applicaton " />
+
+      <List>
+        {activities.map((act: any) => {
+          return <List.Item key={act.id}>{act.title}</List.Item>;
+        })}
+      </List>
     </div>
   );
 }
